9d03fb187b5ec6074861ce980cb582b9b279c0a4
uddinazeem94 <khajaazeemuddin786@gmail.com>
2024-03-14 09:18:51 -0700

    Initial commit added  logo file and welcome message

---
+++ Higher Lower Game Project.py
@@ -0,0 +1,10 @@
+# Higher Lower Game Project
+from art import main_logo, vs_logo
+from replit import clear
+
+# Welcome message
+print(" Welcome... Try out the higher/lower game.")
+print(main_logo)
+
+start = input("Do you want to start? 'y or 'n'.. ")
+
