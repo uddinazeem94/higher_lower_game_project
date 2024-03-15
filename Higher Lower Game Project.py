d0d0764ada866e0a4b09fab955d8cbcb12c283b6
uddinazeem94 <khajaazeemuddin786@gmail.com>
2024-03-14 23:22:53 -0700

    added list function to access dictionary set

--- Higher Lower Game Project.py
+++ Higher Lower Game Project.py
@@ -1,10 +1,62 @@
 # Higher Lower Game Project
-from art import main_logo, vs_logo
+from art import main_logo, vs_logo, celebrities
 from replit import clear
+import random
 
-# Welcome message
-print(" Welcome... Try out the higher/lower game.")
-print(main_logo)
+#function for random choice from list
+def random_choice():
+  return random.choice(list((celebrities)))
 
-start = input("Do you want to start? 'y or 'n'.. ")
+# Looping as a function if user wants to restart
+def higher_lower():
 
+  clear()
+
+  # Welcome message
+  print(" Welcome... Try out the higher/lower game.")
+  print(main_logo)
+
+  # Looping through the list of celebrities
+  score = 0
+  game_over = False
+  option_a = random_choice()
+  value_a = celebrities[option_a]
+
+  # Keeping the previous celebrity to compare with another one
+  while game_over is not True:
+    option_b = random_choice()
+
+    # Generating a random choice if both options are same
+    while option_b == option_a:
+      option_b = random_choice()
+
+    value_b = celebrities[option_b]
+    print(f"Your Score is {score}\n")
+    print(f"Option A- {option_a}")
+    print(vs_logo)
+    print(f"Option B- {option_b}\nWho has more followers on InstaGram?\n")
+    answer = input("type 'a' or 'b'\n").lower()
+    if answer == 'a' and value_a > value_b:
+      score += 1
+      option_a = option_b
+      value_a = value_b
+      clear()
+
+    elif answer == 'b' and value_b > value_a:
+      score +=1
+      option_a = option_b
+      value_a = value_b
+      clear()
+
+    else:
+      print(f"Wrong Answer. Your Score is: {score}\n")
+      game_over = True
+
+
+  run_again = input("Do you want to play again?")
+  if run_again == 'yes' or run_again == 'y':
+    higher_lower()
+
+  else:
+    exit()
+higher_lower()
--- art.py
+++ art.py
@@ -1,4 +1,24 @@
 # Symbols
+# Adding the dictionary here as well
+
+celebrities = {
+  "Kendall Jenner: A famous Tv personality and model": 294000000,
+  "Kim Kardashian: Famous businesswoman and internet sensation": 364000000,
+  "LeBron James: One of the greatest NBA players to ever pick up a basketball": 159000000,
+  "Vin Diesel: An actor, from United States": 102000000,
+  "Billie Elisih: a Musician from United States": 110000000,
+  "Kevin Hart: A comedian, actor from United States": 180000000,
+  "Jennifer Lopez: a musucian and actress from United States": 253000000,
+  "Shawn Mendes: a musician from Canada": 73000000,
+  "Demi Lovato: a Musician and actress from United States": 157000000,
+  "Zendaya: an Actress and musician from United States": 184000000,
+  "Cardi B: a musician from United States": 168000000,
+  "Emma Watson: an actress from United Kingdom": 75300000,
+  "Khloe Kardashian: a reality TV personality and businesswoman from US": 310000000,
+  "Dwayne Johnson: An actor and pro wrestler from the US": 397000000,
+  "Taylor Swift: a musician from United States": 283000000,
+  "9GAG: a social media platform from China": 57200000,
+}
 
 main_logo = """
     __  ___       __             
